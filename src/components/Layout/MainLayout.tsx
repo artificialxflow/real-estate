01db844b6b3874687f0ffe71b9a4c71f333a3412
zedparman <ze.dparman@gmail.com>
2025-09-04 04:24:00 -0700

    Create MainLayout.tsx

---
+++ src/components/Layout/MainLayout.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useState } from 'react';
+import Sidebar from './Sidebar';
+import Header from './Header';
+
+interface MainLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function MainLayout({ children }: MainLayoutProps) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen bg-gray-50">
+      {/* Sidebar */}
+      <Sidebar 
+        isOpen={sidebarOpen} 
+        onClose={() => setSidebarOpen(false)} 
+      />
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col lg:mr-64">
+        <Header onMenuClick={() => setSidebarOpen(true)} />
+        
+        <main className="flex-1 overflow-auto p-6">
+          <div className="max-w-7xl mx-auto">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
